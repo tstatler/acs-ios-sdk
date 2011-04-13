b356e55bbae91c1d9baa4cb684085a5054afdbee
Wei <wei.kong@cocoafish.com>
2011-04-12 23:06:32 -0700

    added missing files

--- src/Cocoafish/Models/CCMessage.h
+++ src/Cocoafish/Models/CCMessage.h
@@ -6,10 +6,25 @@
 //  Copyright 2011 Cocoafish Inc. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import "CCObject.h"
 
+@class CCUser;
 @interface CCMessage : CCObject {
-    
+@private
+    NSString *threadId;
+    NSString *status;
+    NSString *subject;
+    NSString *body;
+    CCUser *from;
+    NSArray *to; // list of users the message was sent to
 }
+
+@property (nonatomic, retain, readonly) NSString *threadId;
+@property (nonatomic, retain, readonly) NSString *status;
+@property (nonatomic, retain, readonly) NSString *subject;
+@property (nonatomic, retain, readonly) NSString *body;
+@property (nonatomic, retain, readonly) CCUser *from;
+@property (nonatomic, retain, readonly) NSArray *to;
 
 @end
